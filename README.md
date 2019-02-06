fad48a4f6861004e79c1a19ee5254ac79c478155
Christoph Flick <christophflick@gmx.de>
2019-02-06 16:02:43 +0100

    Add display and gas sensor

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # node-red-setup
+
 This is the node red setup of my personal IoT environment at home.
 
-# Smart Socket Control
+## Smart Socket Control
+
 One flow controls the smart sockets. In this case TECKIN sockets are used. They are integrated using [node-red-contibr-tuya-smart](https://github.com/hgross/node-red-contrib-tuya-smart).
 
-## Features
+### Features
+
 - On/Off buttons are controlled by the socket as they can be controlled from outside of the application
 - Status message, weather the sockets are online or not using flow variables
 - Power Off-Safety: It is not possible to deactivate a socket if there is to much current flowing
@@ -12,21 +15,29 @@ One flow controls the smart sockets. In this case TECKIN sockets are used. They 
 
 ![Socket Flow](https://github.com/ChFlick/node-red-setup/blob/master/socketFlow.png "Socket Flow")
 
+## Sensor control using MQTT
 
-# Sensor control using MQTT
 The second flow controls MQTT devices. MQTT is handled by a [Mosca MQTT Broker](https://flows.nodered.org/node/node-red-contrib-mqtt-broker).
 
-## Features
+### Features
+
+- Read temperature and humidity
+- Read gas concentration (currently not fully implemented)
 - Display of the values in a gauge (real time) and a chart (5 minute aggregation)
-- Controlling an actuator (simply a LED).
+- Controlling a LED (On/Off)
+- Controlling a Display
 
 ![MQTT Flow](https://github.com/ChFlick/node-red-setup/blob/master/sensorFlow.png "Sensor Flow")
 
-# UI
+## UI
+
 ![Dashboard](https://github.com/ChFlick/node-red-setup/blob/master/dashboard.PNG "Dashboard")
 
-# Sensor/Actuator Setup
-The Sensor/Actuator system uses a Wemos with an ESP8266 microcontroller.
-For temperature and humidity, a DHT22 sensor is being used.
+## Sensor/Actuator Setup
 
-![Sensor Actuator Setup](https://github.com/ChFlick/node-red-setup/blob/master/wemosSetup.jpg "Sensor Actuator Setup")
+The Sensor/Actuator system uses a Wemos with an ESP8266 microcontroller.
+For temperature and humidity, a DHT22 sensor is used, reading through digital input.
+For the natural gas concentration, a MQ-5 sensor is used, reading through analog input.
+
+![Sensor Actuator Setup Front](https://github.com/ChFlick/node-red-setup/blob/master/wemosSetupFront.jpg "Sensor Actuator Setup Front")
+![Sensor Actuator Setup Back](https://github.com/ChFlick/node-red-setup/blob/master/wemosSetupBack.jpg "Sensor Actuator Setup Back")
--- wemos.ino
+++ wemos.ino
@@ -7,15 +7,17 @@
 #include <Wire.h>
 
 #define wifi_ssid "FRITZ!Box 7490"
-#define wifi_password "Flutz2017cc"
+#define wifi_password "pw"
 
 #define mqtt_server "192.168.178.56"
-#define mqtt_user ""      // if exist
-#define mqtt_password ""  //idem
+#define mqtt_user ""     // if exist
+#define mqtt_password "" //idem
 
-#define temperature_topic "sensor/temperature"  //Topic temperature
-#define humidity_topic "sensor/humidity"        //Topic humidity
+#define temperature_topic "sensor/temperature" //Topic temperature
+#define humidity_topic "sensor/humidity"       //Topic humidity
 #define gas_topic "sensor/gas"
+#define display_topic "homeassistant/display"
+#define led_topic "homeassistant/switch1"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -23,59 +25,59 @@
 //Buffer to decode MQTT messages
 char message_buff[100];
 
-long lastMsg = 0;   
+long lastMsg = 0;
 long lastRecu = 0;
-bool debug = true;  //Display log message if True
+bool debug = false; //Display log message if True
 
-#define DHTPIN D4    // DHT Pin 
+#define DHTPIN D4 // DHT Pin
 #define LED_PIN D0
 
 // Un-comment you sensor
-//#define DHTTYPE DHT11       // DHT 11 
-#define DHTTYPE DHT22         // DHT 22  (AM2302)
+//#define DHTTYPE DHT11       // DHT 11
+#define DHTTYPE DHT22 // DHT 22  (AM2302)
 #define OLED_RESET 0  // GPIO0
 
-DHT dht(DHTPIN, DHTTYPE);     
+DHT dht(DHTPIN, DHTTYPE);
 WiFiClient espClient;
 PubSubClient client(espClient);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-void setup() {
-  Serial.begin(9600);     
+void setup()
+{
+  Serial.begin(9600);
   pinMode(A0, INPUT);
-  pinMode(LED_PIN, OUTPUT);     //Pin 2 for LED
-  setup_wifi();           //Connect to Wifi network
-  client.setServer(mqtt_server, 1883);    // Configure MQTT connexion
-  client.setCallback(callback);           // callback function to execute when a MQTT message   
+  pinMode(LED_PIN, OUTPUT);
+  setup_wifi();                        //Connect to Wifi network
+  client.setServer(mqtt_server, 1883); // Configure MQTT connexion
+  client.setCallback(callback);        // callback function to execute when a MQTT message
   dht.begin();
 
-  Serial.println("Initialize display!");
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  {
     Serial.println("Display error!");
-  } else {
-    Serial.println("Display connected!");
   }
-  
-  display.clearDisplay();
+  else
+  {
+    Serial.println("Display connected!");
+    display.display(); // Display spash screen
+    delay(1000);
+  }
 
-  //Add stuff into the 'display buffer'
-  //display.setTextWrap(false);
-  //display.setTextSize(1);
-  //display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.println("teeest.party");
-  //for(int16_t i=max(display.width(),display.height())/2; i>0; i-=5) {
-  //  display.fillTriangle(
-  //      display.width()/2  , display.height()/2-i,
-  //      display.width()/2-i, display.height()/2+i,
-  //      display.width()/2+i, display.height()/2+i, INVERSE);
-  //}
- 
-  display.display(); //output 'display buffer' to screen  
-  display.startscrollleft(0x00, 0x0F); //make display scroll 
+  setup_display();
 }
 
-void setup_wifi() {
+void setup_display()
+{
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(WHITE); // Draw inverse text
+  display.println("Welcome");
+  display.display();
+}
+
+void setup_wifi()
+{
   delay(10);
   Serial.println();
   Serial.print("Connecting to ");
@@ -83,7 +85,8 @@ void setup_wifi() {
 
   WiFi.begin(wifi_ssid, wifi_password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -94,13 +97,18 @@ void setup_wifi() {
   Serial.print(WiFi.localIP());
 }
 
-void reconnect() {
+void reconnect()
+{
 
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("Connecting to MQTT broker ...");
-    if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
+    if (client.connect("ESP8266Client", mqtt_user, mqtt_password))
+    {
       Serial.println("OK");
-    } else {
+    }
+    else
+    {
       Serial.print("KO, error : ");
       Serial.print(client.state());
       Serial.println(" Wait 5 secondes before to retry");
@@ -109,8 +117,10 @@ void reconnect() {
   }
 }
 
-void loop() {
-  if (!client.connected()) {
+void loop()
+{
+  if (!client.connected())
+  {
     reconnect();
   }
   client.loop();
@@ -118,7 +128,8 @@ void loop() {
   long now = millis();
 
   // Send a message every 10 seconds
-  if (now - lastMsg > 1000 * 10) {
+  if (now - lastMsg > 1000 * 10)
+  {
     lastMsg = now;
     // Read humidity
     float h = dht.readHumidity();
@@ -126,66 +137,92 @@ void loop() {
     float t = dht.readTemperature();
 
     // Oh, nothing to send
-    if ( isnan(t) || isnan(h)) {
+    if (isnan(t) || isnan(h))
+    {
       Serial.println("No climate info, please check DH Sensor!");
       return;
     }
 
     float gasRaw = analogRead(A0);
 
-    if( isnan(gasRaw) ) {
+    if (isnan(gasRaw))
+    {
       Serial.println("No gas info, please check MQ-5 Sensor");
       return;
     }
-    
-    float gas = (gasRaw/1023.0) * 3.3;
-  
-    if ( debug ) {
+
+    float gas = (gasRaw / 1023.0) * 3.3;
+
+    if (debug)
+    {
       Serial.print("Temperature : ");
       Serial.print(t);
       Serial.print(" | Humidity : ");
       Serial.print(h);
       Serial.print(" | Gas concentration : ");
       Serial.println(gas);
-    }  
-    client.publish(temperature_topic, String(t).c_str(), true);   // Publish temperature on temperature_topic
-    client.publish(humidity_topic, String(h).c_str(), true);      // and humidity
-    client.publish(gas_topic, String(gas).c_str(), true);      // and gas concentration
+    }
+    client.publish(temperature_topic, String(t).c_str(), true); // Publish temperature on temperature_topic
+    client.publish(humidity_topic, String(h).c_str(), true);    // and humidity
+    client.publish(gas_topic, String(gas).c_str(), true);       // and gas concentration
   }
-  if (now - lastRecu > 100 ) {
+  if (now - lastRecu > 100)
+  {
     lastRecu = now;
-    client.subscribe("homeassistant/switch1");
+    client.subscribe(led_topic);
+    client.subscribe(display_topic);
   }
 }
 
 // MQTT callback function
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
 
   int i = 0;
-  if ( debug ) {
+  if (debug)
+  {
     Serial.println("Message =>  topic: " + String(topic));
-    Serial.print(" | length: " + String(length,DEC));
+    Serial.print(" | length: " + String(length, DEC));
   }
   // create character buffer with ending null terminator (string)
-  for(i=0; i<length; i++) {
+  for (i = 0; i < length; i++)
+  {
     message_buff[i] = payload[i];
   }
   message_buff[i] = '\0';
-  
+
   String msgString = String(message_buff);
-  if ( debug ) {
+  if (debug)
+  {
     Serial.println("Payload: " + msgString);
   }
-  
-  if ( msgString == "ON" ) {
-    digitalWrite(LED_PIN,HIGH);  
-    if ( debug ) {
-      Serial.println("Start LED");
+
+  if (String(topic) == display_topic)
+  {
+    Serial.println("Print to display");
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.print(msgString);
+    display.display();
+  }
+
+  if (String(topic) == led_topic)
+  {
+    if (msgString == "ON")
+    {
+      digitalWrite(LED_PIN, HIGH);
+      if (debug)
+      {
+        Serial.println("Start LED");
+      }
     }
-  } else {
-    digitalWrite(LED_PIN,LOW); 
-    if ( debug ) {
-      Serial.println("Stop LED");
-    } 
+    else
+    {
+      digitalWrite(LED_PIN, LOW);
+      if (debug)
+      {
+        Serial.println("Stop LED");
+      }
+    }
   }
 }
